90440b89dd21bc1f57230e0f7c7f725bd4b67c05
Jan Brejcha <brejchaja@gmail.com>
2019-02-05 18:41:23 +0100

    Implemented easy program to debug crashes of OpenMVG on Macos 10.14.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#define EIGEN_MALLOC_ALREADY_ALIGNED 0
+#define EIGEN_MAX_ALIGN_BYTES 32
+#include "sfm/sfm_data.hpp"
+#include "sfm/sfm_data_io.hpp"
+
+#include <string>
+
+using namespace openMVG::sfm;
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    string sfm_data_path = "/Users/janbrejcha/data/adobe_intern/good_reconstruction/Lake_Tahoe_Brejcha_2017/results/openmvg_global_gps_perspective_pano_6_fov_60_akaze_high/components/43/reconstruction_global/sfm_data.bin";
+    string sfm_data_xml_path ="/Users/janbrejcha/Downloads/sfmtest/sfm_data.json";
+    SfM_Data data;
+
+    if (!openMVG::sfm::Load(data, sfm_data_path,
+
+                                        (ESfM_Data)(VIEWS | EXTRINSICS |
+                                                    INTRINSICS | STRUCTURE)))
+    {
+        throw std::runtime_error("Unable to load sfm_data.");
+    }
+	return 0;
+}
