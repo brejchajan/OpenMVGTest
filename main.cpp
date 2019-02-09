b19c0a5ef65e5729e106cfe4c41ea5ca05f2a5b6
Jan Brejcha <ibrejcha@fit.vutbr.cz>
2019-02-09 22:37:08 +0100

    specify the sfm_data path using argv[1]

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string sfm_data_path = "/Users/janbrejcha/data/adobe_intern/good_reconstruction/Lake_Tahoe_Brejcha_2017/results/openmvg_global_gps_perspective_pano_6_fov_60_akaze_high/components/43/reconstruction_global/sfm_data.bin";
-    string sfm_data_xml_path ="/Users/janbrejcha/Downloads/sfmtest/sfm_data.json";
+    string sfm_data_path = argv[1];
     SfM_Data data;
 
     if (!openMVG::sfm::Load(data, sfm_data_path,
