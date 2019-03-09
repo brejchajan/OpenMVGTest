378b3e1152337323143303f5139d9def6d9be121
Jan Brejcha <brejchaja@gmail.com>
2019-03-09 14:10:18 +0100

    The bug is finally revealed.
    In order to sucessfully compile third party project using OpenMVG as a
    library on MacOS 10.14, OpenMVG needs to use unordered map, which needs
    to be defined also in the third party project (CMake does not take care
    of this). See CMakeLists.txt,
    register_definitions(-DOPENMVG_STD_UNORDERED_MAP).

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "sfm/sfm_data.hpp"
-#include "sfm/sfm_data_io.hpp"
+#include "openMVG/sfm/sfm_data.hpp"
+#include "openMVG/sfm/sfm_data_io.hpp"
 
 #include <string>
 
@@ -12,10 +12,8 @@ int main(int argc, char *argv[])
     string sfm_data_path = argv[1];
     SfM_Data data;
 
-    if (!openMVG::sfm::Load(data, sfm_data_path,
-
-                                        (ESfM_Data)(VIEWS | EXTRINSICS |
-                                                    INTRINSICS | STRUCTURE)))
+    //VIEWS | EXTRINSICS | INTRINSICS | STRUCTURE
+    if (!Load(data, sfm_data_path, (ESfM_Data)(ALL)))
     {
         throw std::runtime_error("Unable to load sfm_data.");
     }
